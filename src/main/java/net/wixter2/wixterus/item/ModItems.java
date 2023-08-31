d51d13c765911641cfd0308621cf6a2404f1f2c7
wixter04 <aymenmlika20@gmail.com>
2023-08-31 22:20:17 +0100

    trying to set tooltips for the armor

--- src/main/java/net/wixter2/wixterus/item/ModItems.java
+++ src/main/java/net/wixter2/wixterus/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.wixter2.wixterus.ArmorMaterial;
 import net.wixter2.wixterus.WixterusMod;
+import net.wixter2.wixterus.item.custom.ModArmorItem;
 import net.wixter2.wixterus.util.WixterusModTier;
 
 public class ModItems {
@@ -23,30 +24,23 @@ public class ModItems {
             () -> new SwordItem(WixterusModTier.WIXTERUS, 9 , -2.4f, new Item.Properties()));
     public  static  final RegistryObject<SwordItem> WIXTERUS_SWORD = ITEMS.register("wixterus_sword",
             () -> new SwordItem(WixterusModTier.WIXTERUS, 1, -2.4f, new Item.Properties()));
-
     public static final RegistryObject<PickaxeItem> WIXTERUS_PICKAXE = ITEMS.register("wixterus_pickaxe",
             () -> new PickaxeItem(WixterusModTier.WIXTERUS, 6 , -2.8f,new Item.Properties().defaultDurability(2789)));
-
-
-
     public  static  final RegistryObject<ShovelItem> WIXTERUS_SHOVEL = ITEMS.register("wixterus_shovel" ,
             () -> new ShovelItem(WixterusModTier.WIXTERUS, 7 , -3 , new Item.Properties().defaultDurability(1946)));
-
-
     public static final RegistryObject<AxeItem> WIXTERUS_AXE = ITEMS.register("wixterus_axe",
             () -> new AxeItem(WixterusModTier.WIXTERUS,11,-3,new Item.Properties().defaultDurability(2249)));
 
+
+
     public static final RegistryObject<ArmorItem> WIXTERUS_HELMET = ITEMS.register("wixterus_helmet",
-            () -> new ArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.HELMET , new Item.Properties()));
-
+            () -> new ModArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.HELMET , new Item.Properties()));
     public static final RegistryObject<ArmorItem> WIXTERUS_CHESTPLATE = ITEMS.register("wixterus_chestplate",
-            () -> new ArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.CHESTPLATE , new Item.Properties()));
-
+            () -> new ModArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.CHESTPLATE , new Item.Properties()));
     public static final RegistryObject<ArmorItem> WIXTERUS_LEGGINGS = ITEMS.register("wixterus_leggings",
-            () -> new ArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.LEGGINGS , new Item.Properties()));
-
+            () -> new ModArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.LEGGINGS , new Item.Properties()));
     public static final RegistryObject<ArmorItem> WIXTERUS_BOOTS = ITEMS.register("wixterus_boots",
-            () -> new ArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.BOOTS , new Item.Properties()));
+            () -> new ModArmorItem(ArmorMaterial.WIXTERUS_ARMOR_MATERIAL,ArmorItem.Type.BOOTS , new Item.Properties()));
     public  static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
